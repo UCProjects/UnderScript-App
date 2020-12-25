c2edd85d9c0fd4d54779dbbba7c9cf2d149049df
feildmaster <feildmaster@users.noreply.github.com>
2020-12-24 22:08:53 -0600

    chore: exit on errors

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const { app } = require('electron');
 const init = require('./src/electron');
 const update = require('./src/underscript');
 
-update().then(init);
+update().then(init).catch((err) => {
+  console.error(err);
+  app.quit();
+});
